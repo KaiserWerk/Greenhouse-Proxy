a35b0ae4615a382b1cb33e62ddb1ce695ef87070
Robin Kaiser <web@r-k.mx>
2021-12-18 22:13:59 +0100

    try multiple COM ports before quitting, send out the actual requests with the measurements

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 	"time"
 
 	"github.com/huin/goserial"
@@ -16,17 +21,41 @@ type Measurement struct {
 	WaterLevel     float64 `json:"water_level"`
 }
 
+const (
+	apiRoute = "/api/v1/receive"
+)
+
 var (
-	m  Measurement
-	cl = &http.Client{Timeout: 2 * time.Second}
+	err     error
+	baseUrl = "http://127.0.0.1:47336/api/v1/receive"
 )
 
 func main() {
+	var (
+		m    Measurement
+		s    io.ReadWriteCloser
+		port string
+		cl   = &http.Client{Timeout: 2 * time.Second}
+	)
+
+	if u := os.Getenv("GREENHOUSE_PROXY_BASEURL"); u != "" {
+		baseUrl = u
+	}
+
 	// Find the device that represents the Arduino serial connection.
-	c := &goserial.Config{Name: `\\.\COM5`, Baud: 9600}
-	s, err := goserial.OpenPort(c)
-	if err != nil {
-		log.Fatalf("could not open port: %s", err.Error())
+	for i := 0; i < 10; i++ {
+		if runtime.GOOS == "windows" {
+			port = fmt.Sprintf(`\\.\COM%d`, i)
+		} else {
+			port = fmt.Sprintf(`/dev/ttyUSB%d`, i)
+		}
+		c := &goserial.Config{Name: port, Baud: 9600}
+		s, err = goserial.OpenPort(c)
+		if err == nil {
+			log.Printf("found port %s\n", port)
+			break
+		}
+		log.Printf("could not open port %s: %s\n", port, err.Error())
 	}
 
 	// When connecting to an older revision Arduino, you need to wait
@@ -43,15 +72,39 @@ func main() {
 
 		if len(b) < 2 {
 			log.Println("got empty line")
+
 			continue
 		}
 
-		err = json.Unmarshal(b[:len(b)-2], &m)
+		err = json.Unmarshal(b[:len(b)-2], &m) // strip the '\n'
 		if err != nil {
 			log.Printf("could not decode JSON '%s': %s\n", b, err.Error())
 			continue
 		}
 
 		log.Printf("Lufttemperatur: %.1f°C, Luftfeuchte: %.1f%%, Wasserstand: %.1f%%\n", m.AirTemperature, m.Humidity, m.WaterLevel)
+		sendMeasurement(cl, &m)
 	}
+}
+
+func sendMeasurement(cl *http.Client, m *Measurement) error {
+	b, err := json.Marshal(*m)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:47336/api/v1/receive", bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+
+	resp, err := cl.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("expected success status code, got %d", resp.StatusCode)
+	}
+
+	return nil
 }
