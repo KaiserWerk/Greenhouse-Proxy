8ccb5ebb7df33c501eafe73defd4121144b4b19d
Robin Kaiser <web@r-k.mx>
2021-12-20 18:18:59 +0100

    Update main.go

--- main.go
+++ main.go
@@ -41,87 +41,86 @@ func main() {
 		cl   = &http.Client{Timeout: 2 * time.Second}
 	)
 
-start:
-
-	if u := os.Getenv("GREENHOUSE_PROXY_BASEURL"); u != "" {
-		log.Printf("found baseUrl env var, setting to '%s'...\n", u)
-		baseUrl = u
-	}
-
-	if k := os.Getenv("GREENHOUSE_PROXY_KEY"); k != "" {
-		log.Printf("found key env var, setting to '%s'...\n", k)
-		key = k
-	}
-
-	if key == "" {
-		log.Fatal("no key found, exiting...")
-	}
-
-	// NOTE: find the device that represents the Arduino serial connection
-	log.Println("Now trying to find open Serial Port...")
-	var i int
-	for i = 0; i < 10; i++ {
-		if runtime.GOOS == "windows" {
-			port = fmt.Sprintf(`\\.\COM%d`, i)
-		} else {
-			port = fmt.Sprintf(`/dev/ttyUSB%d`, i)
-		}
-		log.Printf("attempting to open port %s...\n", port)
-		c := &goserial.Config{Name: port, Baud: 9600}
-		s, err := goserial.OpenPort(c)
-		if err == nil {
-			rwc = s
-			log.Printf("now using port %s\n", port)
-			break
-		}
-		log.Printf("could not open port %s: %s\n", port, err.Error())
-	}
-
-	if rwc == nil {
-		log.Fatal("failed to connect to any serial port")
-	}
-
-	// When connecting to an older revision Arduino, you need to wait
-	// a little while it resets.
-	log.Println("waiting for arduino reset")
-	time.Sleep(1 * time.Second)
-
-	log.Println("starting to read from Serial Port...")
-	br := bufio.NewReader(rwc)
 	for {
-		if emptyLineCounter > maxEmptyLines {
-			emptyLineCounter = 0
-			break
-		}
-		b, err := br.ReadBytes('\n')
-		if err != nil {
-			log.Printf("could not read line: %s\n", err.Error())
-			continue
+		if u := os.Getenv("GREENHOUSE_PROXY_BASEURL"); u != "" {
+			log.Printf("found baseUrl env var, setting to '%s'...\n", u)
+			baseUrl = u
 		}
 
-		if len(b) < 2 {
-			log.Println("got empty line")
-			emptyLineCounter++
-			continue
+		if k := os.Getenv("GREENHOUSE_PROXY_KEY"); k != "" {
+			log.Printf("found key env var, setting to '%s'...\n", k)
+			key = k
 		}
 
-		err = json.Unmarshal(b[:len(b)-2], &m) // strip the '\n'
-		if err != nil {
-			log.Printf("could not decode JSON '%s': %s\n", b, err.Error())
-			continue
+		if key == "" {
+			log.Fatal("no key found, exiting...")
 		}
 
-		log.Printf("Lufttemperatur: %.1f°C, Luftfeuchte: %.1f%%, Wasserstand: %.1f%%\n", m.AirTemperature, m.Humidity, m.WaterLevel)
-		err = sendMeasurement(cl, &m)
-		if err != nil {
-			log.Printf("could not set POST request with measurements: %s", err.Error())
-			continue
+		// NOTE: find the device that represents the Arduino serial connection
+		log.Println("Now trying to find open Serial Port...")
+		var i int
+		for i = 0; i < 10; i++ {
+			if runtime.GOOS == "windows" {
+				port = fmt.Sprintf(`\\.\COM%d`, i)
+			} else {
+				port = fmt.Sprintf(`/dev/ttyUSB%d`, i)
+			}
+			log.Printf("attempting to open port %s...\n", port)
+			c := &goserial.Config{Name: port, Baud: 9600}
+			s, err := goserial.OpenPort(c)
+			if err == nil {
+				rwc = s
+				log.Printf("now using port %s\n", port)
+				break
+			}
+			log.Printf("could not open port %s: %s\n", port, err.Error())
 		}
 
-		log.Println("successfully sent request")
+		if rwc == nil {
+			log.Fatal("failed to connect to any serial port")
+		}
+
+		// When connecting to an older revision Arduino, you need to wait
+		// a little while it resets.
+		log.Println("waiting for arduino reset")
+		time.Sleep(1 * time.Second)
+
+		log.Println("starting to read from Serial Port...")
+		br := bufio.NewReader(rwc)
+		for {
+			if emptyLineCounter > maxEmptyLines {
+				emptyLineCounter = 0
+				break
+			}
+			b, err := br.ReadBytes('\n')
+			if err != nil {
+				log.Printf("could not read line: %s\n", err.Error())
+				continue
+			}
+
+			if len(b) < 2 {
+				log.Println("got empty line")
+				emptyLineCounter++
+				continue
+			}
+
+			err = json.Unmarshal(b[:len(b)-2], &m) // strip the '\n'
+			if err != nil {
+				log.Printf("could not decode JSON '%s': %s\n", b, err.Error())
+				continue
+			}
+
+			//log.Printf("Lufttemperatur: %.1f°C, Luftfeuchte: %.1f%%, Wasserstand: %.1f%%\n", m.AirTemperature, m.Humidity, m.WaterLevel)
+			err = sendMeasurement(cl, &m)
+			if err != nil {
+				log.Printf("could not set POST request with measurements: %s", err.Error())
+				continue
+			}
+
+			log.Println("successfully sent request")
+		}
 	}
 
-	goto start
 }
 
 func sendMeasurement(cl *http.Client, m *Measurement) error {
